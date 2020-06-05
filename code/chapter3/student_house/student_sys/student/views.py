6fc44748c2370b8d0201813b4dc769e4000ff6d9
forrestlmj <18810276930@163.com>
2020-06-05 17:55:36 +0800

    通过form表单提交任务

--- code/chapter3/student_house/student_sys/student/views.py
+++ code/chapter3/student_house/student_sys/student/views.py
@@ -1,9 +1,25 @@
+from django.http import HttpResponse, HttpResponseRedirect
 from django.shortcuts import render
 
 # Create your views here.
+from django.urls import reverse
+
+from student.forms import StudentForm
 from student.models import Student
 
 
 def index(request):
-    students = Student.object.all()
-    return render(request, 'index.html', context={'students': students})
+    students = Student.objects.all()
+    if request.method == 'POST':
+        form = StudentForm(request.POST)
+        if form.is_valid():
+            form.save()
+            return HttpResponseRedirect(reverse('index'))
+    else:
+        form = StudentForm()
+
+    context = {
+        'students': students,
+        'form': form
+    }
+    return render(request, 'index.html', context=context)
