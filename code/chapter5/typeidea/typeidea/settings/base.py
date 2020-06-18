2bdfe8ae44335fefbfa2a789bcff89b84658e1f0
forrestlmj <18810276930@163.com>
2020-06-18 13:14:07 +0800

    blog与post

--- code/chapter5/typeidea/typeidea/settings/base.py
+++ code/chapter5/typeidea/typeidea/settings/base.py
@@ -31,6 +31,9 @@ ALLOWED_HOSTS = []
 # Application definition
 
 INSTALLED_APPS = [
+    'blog',
+    'config',
+    'post',
     'django.contrib.admin',
     'django.contrib.auth',
     'django.contrib.contenttypes',
