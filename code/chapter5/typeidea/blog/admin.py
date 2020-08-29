5a3ae6b38c162b373a547a3284b1eec358b574cf
forrestlmj <18810276930@163.com>
2020-08-29 22:05:01 +0800

    Add PostAdmin.

--- code/chapter5/typeidea/blog/admin.py
+++ code/chapter5/typeidea/blog/admin.py
@@ -2,8 +2,10 @@ from django.contrib import admin
 
 # Register your models here.
 from django.contrib import admin
+from django.urls import reverse
+from django.utils.html import format_html
 
-from blog.models import Category, Tag
+from blog.models import Category, Tag, Post
 
 
 @admin.register(Category)
@@ -16,7 +18,6 @@ class CategoryAdmin(admin.ModelAdmin):
         return super(CategoryAdmin, self).save_model(request, obj, form, change)
 
 
-
 @admin.register(Tag)
 class TagAdmin(admin.ModelAdmin):
     list_display = ('name', 'status', 'created_time')
@@ -25,4 +26,40 @@ class TagAdmin(admin.ModelAdmin):
     def save_model(self, request, obj, form, change):
         obj.owner = request.user
         return super(TagAdmin, self).save_model(request, obj, form, change)
+
+
+@admin.register(Post)
+class PostAdmin(admin.ModelAdmin):
+    list_display = [
+        'title', 'category', 'status',
+        'created_time', 'operator'
+    ]
+    list_display_links = []
+
+    list_filter = ['category', ]
+    search_fields = ['title', 'category__name']
+
+    actions_on_top = True
+    actions_on_bottom = True
+
+    save_on_top = True
+
+    fields = (
+        ('category', 'title'),
+        'desc',
+        'status',
+        'content',
+        'tag',
+    )
+
+    def operator(self, obj):
+        return format_html(
+            '<a href="{}">bianji</a>',
+            reverse('admin:blog_post_change', args=(obj.id,) )
+        )
+    operator.short_description = '操作'
+
+    def save_model(self, request, obj, form, change):
+        obj.owner = request.user
+        return super(PostAdmin, self).save_model(request, obj, form, change)
 
