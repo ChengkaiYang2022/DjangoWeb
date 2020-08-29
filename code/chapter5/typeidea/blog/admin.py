9649bbec82bf0eab3f2764df35a70b11bbb4e0e8
forrestlmj <18810276930@163.com>
2020-08-29 21:04:35 +0800

    A simple admin file.Just register the model on admin.

--- code/chapter5/typeidea/blog/admin.py
+++ code/chapter5/typeidea/blog/admin.py
@@ -1,3 +1,18 @@
 from django.contrib import admin
 
 # Register your models here.
+from django.contrib import admin
+
+from blog.models import Category, Tag
+
+
+@admin.register(Category)
+class CategoryAdmin(admin.ModelAdmin):
+    list_display = ('name', 'status', 'is_nav', 'created_time')
+    fields = ('name', 'is_nav', 'owner', 'status')
+
+
+@admin.register(Tag)
+class TagAdmin(admin.ModelAdmin):
+    list_display = ('name', 'status', 'created_time')
+    fields = ('name', 'status')
