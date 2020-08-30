ab74a81364cf71812fac657b635cec54f5c663ca
forrestlmj <18810276930@163.com>
2020-08-30 21:47:26 +0800

    配置Category的queryset设置。

--- code/chapter5/typeidea/blog/admin.py
+++ code/chapter5/typeidea/blog/admin.py
@@ -28,15 +28,31 @@ class TagAdmin(admin.ModelAdmin):
         return super(TagAdmin, self).save_model(request, obj, form, change)
 
 
+class CategoryOwnerFilter(admin.SimpleListFilter):
+    """ 自定义过滤器只展示当前用户分类"""
+
+    title = '分类过滤器'
+    parameter_name = 'owner_category'
+
+    def lookups(self, request, model_admin):
+        return Category.objects.filter(owner=request.user).values_list('id', 'name')
+
+    def queryset(self, request, queryset):
+        category_id = self.value()
+        if category_id:
+            return queryset.filter(category_id=self.value())
+        return queryset
+
+
 @admin.register(Post)
 class PostAdmin(admin.ModelAdmin):
     list_display = [
         'title', 'category', 'status',
-        'created_time', 'operator'
+        'created_time', 'owner', 'operator'
     ]
     list_display_links = []
 
-    list_filter = ['category', ]
+    list_filter = [CategoryOwnerFilter]
     search_fields = ['title', 'category__name']
 
     actions_on_top = True
@@ -54,7 +70,7 @@ class PostAdmin(admin.ModelAdmin):
 
     def operator(self, obj):
         return format_html(
-            '<a href="{}">bianji</a>',
+            '<a href="{}">编辑</a>',
             reverse('admin:blog_post_change', args=(obj.id,) )
         )
     operator.short_description = '操作'
@@ -63,3 +79,6 @@ class PostAdmin(admin.ModelAdmin):
         obj.owner = request.user
         return super(PostAdmin, self).save_model(request, obj, form, change)
 
+    def get_queryset(self, request):
+        qs = super(PostAdmin, self).get_queryset(request)
+        return qs.filter(owner=request.user)
