cb71c75a9847d5ccaf4a1fa6c1e14db96f8397b3
forrestlmj <18810276930@163.com>
2020-09-01 17:10:55 +0800

    code 重名为pythoncode。 code命名会导致pycharm不可用。

--- code/chapter2/wsgi_example/app.py
+++
@@ -1,9 +0,0 @@
-# coding:utf-8
-
-
-def simple_app(environ, start_response):
-    """web应用"""
-    status = '200 OK'
-    response_heads = [('Content-type', 'text/plain')]
-    start_response(status, response_heads)
-    return [b'Hello world!\n']
